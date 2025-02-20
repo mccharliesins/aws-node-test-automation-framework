9efe46a6dbb9b5a2d9d0ad6d8ba0c0c97b0a9bde
mccharliesins <vkyd1909@gmail.com>
2025-02-20 09:13:04 -0500

    feat: add auth token generation stub

--- utils/aws-helper.ts
+++ utils/aws-helper.ts
@@ -6,3 +6,11 @@ import * as path from 'path';
 export const generateAuthToken = async (): Promise<string> => {
     // Integration with Cognito Identity Provider
     // const client = new CognitoIdentityProviderClient({ region: "us-east-1" });
+    // const command = new InitiateAuthCommand({ ... });
+    // const response = await client.send(command);
+    // return response.AuthenticationResult?.AccessToken || "";
+
+    return "mock-jwt-token-ey1234567890";
+};
+
+export const uploadReportToS3 = async (bucketName: string, filePath: string): Promise<void> => {
